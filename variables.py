49b959115426989459ae17275fe432b6d063bcba
Becomeadoctor <sneakyninja389@gmail.com>
2025-08-31 23:07:05 -0400

    Initial Commit

---
+++ variables.py
@@ -0,0 +1,29 @@
+number1=5
+number2=6
+result = number1*number2
+print("The answer to your query is {}".format(result))
+
+number1=8
+number2=3
+result = number1/number2
+print("The answer to your query is {}".format(result))
+
+number1=8
+number2=3
+result = number1//number2
+print("The answer to your query is {}".format(result))
+
+number1=8
+number2=3
+result = number1 % number2
+print("The answer to your query is {}".format(result))
+
+number1=4200000
+number2=23090
+result = number1 + number2
+print("The answer to your query is {}".format(result))
+
+number1=4200000
+number2=7633
+result = number1 - number2
+print("The answer to your query is {}".format(result))
